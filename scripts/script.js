a5e99d5066671c388dd2a98f7a17ba80a6346f95
Siarhei Kazakevich <dianrom@tut.by>
2025-11-02 20:31:14 +0300

    feat: add sounds for click

--- scripts/script.js
+++ scripts/script.js
@@ -9,13 +9,13 @@ document.body.appendChild(title);
 document.body.appendChild(drumContainer);
 
 const drums = [
-  { key: 'A', sound: 'Kick' },
-  { key: 'S', sound: 'Snare' },
-  { key: 'D', sound: 'Hi-Hat' },
-  { key: 'F', sound: 'Tom' },
-  { key: 'G', sound: 'Clap' },
-  { key: 'H', sound: 'Crash' },
-  { key: 'J', sound: 'Ride' },
+  { key: 'A', sound: 'Kick', file: './sounds/kick_drum_hard_3.wav' },
+  { key: 'S', sound: 'Snare', file: './sounds/snare2.wav' },
+  { key: 'D', sound: 'Hi-Hat', file: './sounds/hi-hat.wav' },
+  { key: 'F', sound: 'Tom', file: './sounds/tom.wav' },
+  { key: 'G', sound: 'Clap', file: './sounds/clap.wav' },
+  { key: 'H', sound: 'Crash', file: './sounds/crash.wav' },
+  { key: 'J', sound: 'Ride', file: './sounds/ride.wav' },
 ];
 
 drums.forEach(drum => {
@@ -23,5 +23,20 @@ drums.forEach(drum => {
   button.className = 'drum';
   button.textContent = `${drum.key}-${drum.sound}`;
   button.dataset.key = drum.key;
+  button.dataset.sound = drum.file;
   drumContainer.appendChild(button);
+
+  button.addEventListener('click', () => playSound(drum.file));
 });
+
+function playSound(soundPath) {
+  const audio = new Audio(soundPath);
+  audio.currentTime = 0;
+  audio.play();
+}
+drumContainer.addEventListener('click', e => {
+  if (!e.target.classList.contains('drum')) return;
+  const soundPath = e.target.dataset.sound;
+  playSound(soundPath);
+  /*flashButton(e.target.dataset.key);*/
+})
